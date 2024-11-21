80ea1ab2c8ad482d6e82b13070209a805cfbbea2
Gustavo Bernal <gustavober98@gmail.com>
2024-11-21 15:17:52 -0500

    docs: Docs of plague and family  added successfullly

--- obj/Debug/net8.0/PlantasBackend.AssemblyInfo.cs
+++ obj/Debug/net8.0/PlantasBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlantasBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77028eced272915f04fe6f713f472056d1015e41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7666a6d96c6ecffa37a2f134b498491ad38616b")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlantasBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlantasBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
