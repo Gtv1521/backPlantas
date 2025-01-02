6f2bb695e988f42c9187aefdea2ec957b096e149
Gustavo Bernal <gustavober98@gmail.com>
2025-01-02 14:33:47 -0500

    feat: added request to search family for name return id, name

--- obj/Debug/net8.0/PlantasBackend.AssemblyInfo.cs
+++ obj/Debug/net8.0/PlantasBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlantasBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+839dfe570349fd07b7170e0c038a1997c4342c87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd502302edea0dde69294ab6d7efbf7e225e2a92")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlantasBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlantasBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
